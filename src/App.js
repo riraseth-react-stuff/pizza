832d11f72f145241bfb153fb2f5812d0414fd796
Łukasz Zabuski <riraseth@gmail.com>
2020-03-26 22:54:33 +0100

    fonts, global style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+body {
+  font-family: 'Open Sans', sans-serif;
+}
+
+h1, h2, h3 {
+  font-family: 'Righteous', cursive;
+}
+
+`;
 
 function App() {
-  return <div className="">hello pizza</div>;
+  return (
+    <React.Fragment>
+      <GlobalStyle></GlobalStyle>
+      <h1>Slice Line</h1>
+      <div className="">hello pizza</div>
+    </React.Fragment>
+  );
 }
 
 export default App;
