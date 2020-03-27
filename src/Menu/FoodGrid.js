966de8997079193f809c9a6e68370c015557d200
Łukasz Zabuski <riraseth@gmail.com>
2020-03-27 23:10:02 +0100

    Update FoodGrid.js

--- src/Menu/FoodGrid.js
+++ src/Menu/FoodGrid.js
@@ -21,11 +21,15 @@ export const Food = styled(StyledTitle)`
   background-position: center;
   background-size: cover;
   filter: contrast(75%);
+  margin-top: 5px;
   border-radius: 7px;
   box-shadow: 0px 0px 10px 0px grey;
-  transition: 0.2s linear;
+  transition: 0.15s linear;
   &:hover {
     cursor: pointer;
-    opacity: 0.7;
+    filter: contrast(100%);
+    margin-top: 0px;
+    margin-bottom: 5px;
+    box-shadow: 0px 5px 10px 0px grey;
   }
 `;
